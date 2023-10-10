5b40453df331973c0348030bd248b0454928fc11
Algor Fernand <algorfernand014@gmail.com>
2023-10-10 17:30:37 +0200

    refactor app to use createBrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import RootLayout from "./layouts/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+);
 function App() {
-  const activeLink = "bg-[#bc4123] rounded-md ring-1 ring-white px-2";
-  return (
-    <BrowserRouter>
-      <header>
-        <nav className="flex justify-between">
-          <h1 className="font-bold border-b-2 border-[#bc4123]">Jobarouter</h1>
-          <div className="flex gap-5">
-            <NavLink
-              className={({ isActive }) => (isActive ? activeLink : "")}
-              to="/"
-            >
-              Home
-            </NavLink>
-            <NavLink
-              className={({ isActive }) => (isActive ? activeLink : "")}
-              to="about"
-            >
-              About
-            </NavLink>
-          </div>
-        </nav>
-      </header>
-      <main className="mt-14">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
